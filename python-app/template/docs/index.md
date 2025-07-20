f9705bf31675979cdde1a580a5d2d62cc5f5d1bd
minhpham95 <chauchau310897@gmail.com>
2025-07-20 11:55:36 +0700

    add python tempplate

---
+++ python-app/template/docs/index.md
@@ -0,0 +1,11 @@
+# Documents for ${{values.app_name}}
+
+This application has 2 endpoints:
+- `/api/v1/info`
+- `/api/v1/healthz`
+Here you can expand of what each of these endpoint do.
+
+
+# How to access the app
+
+You can access the app by accessing this URL: `${{values.app_name}}-${{values.app_env}}.plminh.com/api/v1/info`, `${{values.app_name}}-${{values.app_env}}.plminh.com/api/v1/healthz`
---
+++ python-app/template/src/app.py
@@ -0,0 +1,38 @@
+# Importing flask module in the project is mandatory
+# An object of Flask class is our WSGI application.
+from flask import Flask, jsonify
+import datetime
+import socket
+
+# Flask constructor takes the name of 
+# current module (__name__) as argument.
+app = Flask(__name__)
+
+# The route() function of the Flask class is a decorator, 
+# which tells the application which URL should call 
+# the associated function.
+@app.route('/api/v1/info')
+# ‘/’ URL is bound with hello_world() function.
+def info():
+    return jsonify({
+        'time': datetime.datetime.now().strftime("%I:%M:%S%p on %B %d, %Y"),
+        'hostname': socket.gethostname(),
+        'message': 'Code 200: Good job !!!',
+        'deployed_on': 'kubernetes local cluster',
+        'env': '${{values.app_env}}',
+        'app_name': '${{values.app_name}}'
+    })
+
+@app.route('/api/v1/healthz')
+# ‘/’ URL is bound with hello_world() function.
+def healthz():
+    return jsonify({
+        'status': 'up'
+    }), 200
+
+# main driver function
+if __name__ == '__main__':
+
+    # run() method of Flask class runs the application 
+    # on the local development server.
+    app.run(host="0.0.0.0")
